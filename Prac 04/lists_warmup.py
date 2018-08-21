aa81180bfeb29a40eb562a6fde51206d7a97b656
Lucy Smith <lucy.smith2@my.jcu.edu.au>
2018-08-21 12:44:14 +1000

    Display income report

---
+++ Prac 04/lists_warmup.py
@@ -0,0 +1,31 @@
+"""
+numbers[0] = 3
+numbers[-1] = 
+numbers[3] = 1
+numbers[:-1] = 9 
+numbers[3:4] = 1, 5
+5 in numbers = 
+7 in numbers = 
+"3" in numbers =
+numbers + [6, 5, 3] = numbers = [3, 1, 3, 5, 9, 2, 6, 5, 3]
+"""
+
+
+numbers = [3, 1, 3, 1, 5, 9, 2]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+
+print(numbers[2:])
+
+
+
+
+
+
+
+
+
+
+
+
